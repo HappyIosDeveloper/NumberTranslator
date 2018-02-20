0e84cd414d9eddaf36dc3fad0f033ccef278d4c1
Alfredo <lovlyboy@gmail.com>
2018-02-20 06:45:36 +0330

    Enhanced functions.

--- NumberTranslator.swift
+++ NumberTranslator.swift
@@ -8,7 +8,6 @@
 
 extension String {
     
-    
     public func toPersianNumber() -> String {
         var response = self
         var enNumber = ["0","1","2","3","4","5","6","7","8","9"]
@@ -16,22 +15,24 @@ extension String {
         for i in 0 ..< 10 {
             response = response.replace(enNumber[i], withString: faNumber[i])
         }
-        
         return response
     }
     
     public func toEnglishNumber() -> String {
         var response = self
         var enNumber = ["0","1","2","3","4","5","6","7","8","9"]
-        var faNumber = ["۰","۱","۲","۳","۴","۵","۶","۷","۸","۹"] // ۱۲۳۴۵۶۷۸۹۰  ["٠","١","٢","٣","٤","٥","٦","٧","٨","٩"]
+        var faNumber = ["۰","۱","۲","۳","۴","۵","۶","۷","۸","۹"] 
         var faNumber2 = ["٠","١","٢","٣","٤","٥","٦","٧","٨","٩"]
+        var faNumber3 = ["٠","١","٢","٣","٤","٥","٦","٧","٨","٩"]
         
         for i in 0..<10 {
             response = response.replace(faNumber[i], withString: enNumber[i])
         }
-        
         for i in 0..<10 {
             response = response.replace(faNumber2[i], withString: enNumber[i])
+        }
+        for i in 0..<10 {
+            response = response.replace(faNumber3[i], withString: enNumber[i])
         }
         
         return response
@@ -48,7 +49,6 @@ extension Int {
         for i in 0 ..< 10 {
             response = response.replace(enNumber[i], withString: faNumber[i])
         }
-        
         return response
     }
     
@@ -56,8 +56,17 @@ extension Int {
         var response = String(self)
         var enNumber = ["0","1","2","3","4","5","6","7","8","9"]
         var faNumber = ["٠","١","٢","٣","٤","٥","٦","٧","٨","٩"]
+        var faNumber2 = ["٠","١","٢","٣","٤","٥","٦","٧","٨","٩"]
+        var faNumber3 = ["٠","١","٢","٣","٤","٥","٦","٧","٨","٩"]
+        
         for i in 0 ..< 10 {
             response = response.replace(faNumber[i], withString: enNumber[i])
+        }
+        for i in 0 ..< 10 {
+            response = response.replace(faNumber2[i], withString: enNumber[i])
+        }
+        for i in 0 ..< 10 {
+            response = response.replace(faNumber3[i], withString: enNumber[i])
         }
         
         return response
